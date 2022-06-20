59997361b67c30469e4d2e87349ae05d3337511b
danielkaradaliev <daniel_karadaliev@abv.bg>
2022-06-20 15:38:47 +0300

    task 06 solved

---
+++ 03_Stacks_Queues_Tuples_and_Sets/06_Paint_Colors.py
@@ -0,0 +1,52 @@
+from collections import deque
+
+
+def add_main_color(main_color):
+    colors_found.append(main_color)
+
+    for secondary_color in secondary_colors_found:
+        add_secondary_color(secondary_color)
+
+
+def add_secondary_color(secondary_color):
+    if (secondary_color == "orange" and ("red" in colors_found and "yellow" in colors_found)) or \
+            (secondary_color == "purple" and ("red" in colors_found and "blue" in colors_found)) or \
+            (secondary_color == "green" and ("yellow" in colors_found and "blue" in colors_found)) and \
+            secondary_color not in colors_found:
+        colors_found.insert(secondary_colors_found[secondary_color], secondary_color)
+
+
+strings = deque(input().split())
+
+main_colors_to_find = {"red", "yellow", "blue"}
+secondary_colors_to_find = {"orange", "purple", "green"}
+colors_found = []
+secondary_colors_found = {}
+
+while strings:
+    substrings_to_check = []
+    if len(strings) == 1:
+        substrings = [strings.popleft()]
+        substrings_to_check.append(substrings[0])
+    else:
+        substrings = [strings.popleft(), strings.pop()]
+        substrings_to_check.append(substrings[0] + substrings[1])
+        substrings_to_check.append(substrings[1] + substrings[0])
+
+    for substring in substrings_to_check:
+        if substring in main_colors_to_find:
+            add_main_color(substring)
+            break
+        elif substring in secondary_colors_to_find:
+            secondary_color_index = len(colors_found)
+            secondary_colors_found[substring] = secondary_color_index
+            add_secondary_color(substring)
+            break
+    else:
+        index = len(strings) // 2
+        for x in substrings[::-1]:
+            substring_to_insert = x[:-1]
+            if len(substring_to_insert):
+                strings.insert(index, substring_to_insert)
+
+print(colors_found)
