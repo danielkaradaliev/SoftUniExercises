c29056ec4bec77b9a29c83cdadeca3b148f7b3dc
danielkaradaliev <daniel_karadaliev@abv.bg>
2022-06-21 12:19:40 +0300

    added a square dimension argument to the is_square_matrix function

--- 04_Multidimensional_Lists/03_2x2_Squares_in_Matrix.py
+++ 04_Multidimensional_Lists/03_2x2_Squares_in_Matrix.py
@@ -1,17 +1,20 @@
-def is_square_matrix(matrix, row_index, col_index):
-    if matrix[row_index][col_index] == matrix[row_index + 1][col_index] == \
-       matrix[row_index][col_index + 1] == matrix[row_index + 1][col_index + 1]:
-        return True
-    return False
+def is_square_matrix(matrix, row_index, col_index, square_dimension):
+    symbol = matrix[row_index][col_index]
+    for r in range(row_index, row_index + square_dimension):
+        for c in range(col_index, col_index + square_dimension):
+            if matrix[r][c] != symbol:
+                return False
+    return True
 
 
 rows, columns = [int(x) for x in input().split()]
 my_matrix = [input().split() for y in range(rows)]
+square_dimension = 2
 square_matrices = 0
 
-for r_index in range(rows - 1):
-    for c_index in range(columns - 1):
-        if is_square_matrix(my_matrix, r_index, c_index):
+for r_index in range(rows - square_dimension + 1):
+    for c_index in range(columns - square_dimension + 1):
+        if is_square_matrix(my_matrix, r_index, c_index, square_dimension):
             square_matrices += 1
 
 print(square_matrices)
